498cecd69e1f9e2952289bcdb317b32e318f0ebd
Luis Parravicini <lparravi@gmail.com>
2011-02-14 09:02:42 -0300

    copy empty db if file doesnt exist

--- fb.rb
+++ fb.rb
@@ -1,5 +1,6 @@
 
 require 'mechanize'
+require 'fileutils'
 require 'sqlite3'
 
 #
@@ -7,9 +8,18 @@ require 'sqlite3'
 # leaf node so there's no need to check for duplicates
 #
 
+module BaseStore
+  def self.open_db(fname)
+    unless File.exists?(fname)
+      FileUtils.cp(fname.gsub(/\.db/, '_empty.db'), fname)
+    end
+    SQLite3::Database.new(fname)
+  end
+end
+
 class PeopleStore
   def initialize
-    @db = SQLite3::Database.new('people.db')
+    @db = BaseStore.open_db('people.db')
   end
 
   def update(leaves)
@@ -56,7 +66,7 @@ end
 
 class QueueStore
   def initialize
-    @db = SQLite3::Database.new('queue.db')
+    @db = BaseStore.open_db('queue.db')
   end
 
   def reset_status
@@ -74,14 +84,6 @@ class QueueStore
     queue = 0
 
     @db.transaction do |db|
-      unless leaves.empty?
-        stmt = @db.prepare('INSERT OR IGNORE INTO people (url, name) VALUES(?,?)')
-        leaves.each do |leaf|
-          stmt.execute(leaf[:url], leaf[:title])
-          people += @db.changes
-        end
-      end
-
       unless nodes.empty?
         stmt = @db.prepare('INSERT OR IGNORE INTO queue (url) VALUES(?)')
         nodes.each do |node|
@@ -103,33 +105,6 @@ class QueueStore
   def queue_size
     @db.execute('SELECT COUNT(1) FROM queue WHERE status IS NULL').first.first
   end
-
-end
-
-def download(agent, url)
-  retries = 0
-  page = nil
-  begin
-    print url
-    return agent.get(url)
-  rescue Errno::ECONNREFUSED, Timeout::Error, Net::HTTPInternalServerError,
-  Errno::ETIMEDOUT
-    retries += 1
-    puts " Error: #{$!.message}"
-    if retries > 3
-      puts "Too many retries"
-      exit
-    end
-    sleep 15
-    retry
-  end
-end
-
-def parse(page)
-  links = page.links.select { |link| link.href =~ %r{/people/.} }
-  links.map! { |link| { :url => link.href, :title => link.text } }
-
-  links.partition { |link| link[:url] =~ %r{/directory/} }
 end
 
 class Store
@@ -145,39 +120,95 @@ class Store
   def update(id, nodes, leaves)
     [ @queue.update(id, nodes), @people.update(leaves) ]
   end
+
+  def next; @queue.next; end
+
+  def queue_size; @queue.queue_size; end
 end
+
+
+class Downloader
+  def initialize
+  end
+
+  def get(url)
+      agent = Mechanize.new
+      agent.user_agent_alias = 'Windows IE 7'
+
+      parse(download(agent, url))
+  end
+
+  protected
+
+  def download(agent, url)
+    retries = 0
+    page = nil
+    begin
+      print url
+      return agent.get(url)
+    rescue Errno::ECONNREFUSED, Timeout::Error, Net::HTTPInternalServerError,
+    Errno::ETIMEDOUT
+      retries += 1
+      puts " Error: #{$!.message}"
+      if retries > 3
+        puts "Too many retries"
+        exit
+      end
+      sleep 15
+      retry
+    end
+  end
+
+  def parse(page)
+    links = page.links.select { |link| link.href =~ %r{/people/.} }
+    links.map! { |link| { :url => link.href, :title => link.text } }
+
+    links.partition { |link| link[:url] =~ %r{/directory/} }
+  end
+end
+
+class Harvester
+  def initialize(db)
+    @downloader = Downloader.new
+    @db = db
+  end
+
+  def run
+    n = 0
+    while true do
+      result = @db.next
+      if result.nil?
+        puts "No more URLs"
+        break
+      end
+
+      id = result.first
+      url = result.last
+        
+      nodes, leaves = @downloader.get(url)
+      people, queue = @db.update(id, nodes, leaves)
+
+      print_stats(queue, nodes, people, leaves)
+
+      n += 1
+      puts "queue: #{@db.queue_size}" if n % 50 == 0
+    end
+  end
+
+  protected
+
+  def print_stats(queue, nodes, people, leaves)
+    buf = "\t"
+    buf += "#{queue}/" if queue != nodes.size
+    buf += "#{nodes.size}\t"
+    buf += "#{people}/" if people != leaves.size
+    buf += "#{leaves.size}"
+    puts buf
+  end
+end
+
 
 $db = Store.new
 $db.reset_status
 
-
-
-n = 0
-while true do
-  result = $db.next
-  if result.nil?
-    puts "No more URLs"
-    break
-  end
-
-  id = result.first
-  url = result.last
-    
-  agent = Mechanize.new
-  agent.user_agent_alias = 'Windows IE 7'
-
-  page = download(agent, url)
-  nodes, leaves = parse(page)
-
-  people, queue = $db.update(id, nodes, leaves)
-
-  buf = "\t"
-  buf += "#{queue}/" if queue != nodes.size
-  buf += "#{nodes.size}\t"
-  buf += "#{people}/" if people != leaves.size
-  buf += "#{leaves.size}"
-  puts buf
-
-  n += 1
-  puts "queue: #{$db.queue_size}" if n % 50 == 0
-end
+Harvester.new($db).run
